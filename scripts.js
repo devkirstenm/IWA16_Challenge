291326d1e643660dc62b0f862925451eed8b953d
Kirsten <kirsten.muller99@gmail.com>
2023-04-28 09:10:06 +0200

    Updated

--- scripts.js
+++ scripts.js
@@ -77,40 +77,54 @@ const [{date, time}] = races.reverse() // added [] and {} around date/time
 
 const fragment = document.createDocumentFragment();
 
-const title = document.createElement(h2); // added const to variable title
+const title = document.createElement('h2'); // added const to variable title & made h2 a string element which tells the browser that its a string representing a HTML element
 title.textContent = id; // set the text of the element
 fragment.appendChild(title);
 
 const list = document.createElement(dl);
 
 const day = date.getDate();
-const month = MONTHS[date.month];
-const year = date.year;
+const month = date.getMonth(); // changed to date.month to correct
+const year = date.getYear(); // changed to correct syntax
 
 const [first, second, third, fourth] = time; // changed timeasarray to time as its already an array and added const to variable
 const total = first + second + third + fourth; // added const
 
 const hours = total / 60;
-const minutes = total / hours / 60;
+const minutes = (total - h(ours / 60)); // changed format
 
 // correct format for variables
 const athleteInfo = document.getElementById("athlete-info");
-athleteInfo.list.innerHTML = /* html */ ` 
+const NM372 = /* html */ ` 
     <dt>Athlete</dt>
-    <dd>${firstName} ${surname}</dd>  
+    <dd>${response.data.NM372.firstName} ${response.data.NM372.surname}</dd>  
 
     <dt>Total Races</dt>
-    <dd>${races.length}</dd> 
+    <dd>${response.data.NM372.races.length}</dd> 
 
     <dt>Event Date (Latest)</dt>
-    <dd>${day} ${month} ${year}</dd>
+    <dd>${day} ${month} ${year}</dd> 
+
+    <dt>Total Time (Latest)</dt>
+    <dd>${hours.padStart(2, '0')} ${minutes}</dd>
+`;
+
+const SV782 = /* html */ ` 
+    <dt>Athlete</dt>
+    <dd>${response.data.SV782.firstName} ${response.data.SV782.surname}</dd>  
+
+    <dt>Total Races</dt>
+    <dd>${response.data.SV782.races.length}</dd> 
+
+    <dt>Event Date (Latest)</dt>
+    <dd>${day} ${month} ${year}</dd> 
 
     <dt>Total Time (Latest)</dt>
     <dd>${hours.padStart(2, '0')} ${minutes}</dd>
 `;
 
 fragment.appendChild(list);
-}
-const {NM372, SV782} = data.response.data // added other variables nested inside to properly access the athletes
-document.querySelector(NM372).appendChild(createHtml(NM372));
-document.querySelector(SV782).appendChild(createHtml(SV782));
+
+let {NM372, SV782} = data.response.data // added other variables nested inside to properly access the athletes data
+const athlete1 = document.querySelector('#NM372').appendChild(innerHTML = createHtml(NM372)); // added document.createHTMl
+const athlete2 = document.querySelector('#SV782').appendChild(innerHTML = createHtml(SV782));
